9278afe2ef538d0572d11b6b4c02ab6af69458c8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-11 10:19:15 +0900

    echo導入

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
 	gorm.io/driver/postgres v1.0.6
 	gorm.io/gorm v1.20.10
 )
--- src/main.go
+++ src/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"poiyo-be/src/database"
 	"poiyo-be/src/environment"
 	"poiyo-be/src/model"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
 	dotenv := environment.Load()
 	db := database.Connect(dotenv)
-
 	// usersテーブルから値を取得して出力.
 	users := []model.User{}
 	db.Find(&users)
 	for _, user := range users {
 		fmt.Printf("%s %s\n", user.UserId, user.CreatedAt)
 	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
