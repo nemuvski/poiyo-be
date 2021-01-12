f7322ba779d4f27110c7215b03bfe423421eb5f8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-12 23:02:26 +0900

    メールアドレスを追加

--- src/api/account.go
+++ src/api/account.go
@@ -20,11 +20,16 @@ func PostAccount() echo.HandlerFunc {
 		// アカウントデータが存在するか（登録済みか）を確認.
 		result := tx.Where("service_type = ? AND service_id = ?", m.ServiceType, m.ServiceId).First(&account)
 		if result.RowsAffected > 0 {
-			return c.JSON(http.StatusCreated, account)
+			// もしメールアドレスが変わっていた場合は更新する.
+			if account.Email != m.Email {
+				tx.Model(&account).UpdateColumn("email", m.Email)
+			}
+			return c.JSON(http.StatusOK, account)
 		}
 
 		account.ServiceType = m.ServiceType
 		account.ServiceId = m.ServiceId
+		account.Email = m.Email
 		tx.Create(&account)
 
 		return c.JSON(http.StatusCreated, account)
