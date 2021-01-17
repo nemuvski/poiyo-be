5cc56e89f25465a92338a41034a2e18298789c7a
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-17 14:03:03 +0900

    board,commentのモデル作成

---
+++ src/model/board.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Board struct {
+	BoardId        string    `json:"board_id" gorm:"primaryKey;not null;type:uuid;default:gen_random_uuid()"`
+	Title          string    `json:"title" gorm:"not null;type:varchar(200)"`
+	Body           string    `json:"body" gorm:"not null;type:varchar(1000)"`
+	OwnerAccountId string    `json:"owner_account_id" gorm:"not null;type:uuid"`
+	CreatedAt      time.Time `json:"created_at" gorm:"not null;default:current_timestamp"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
