e76928a792a67212a990b6f1d89dd05dfb6e03de
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-24 19:29:29 +0900

    authのアノテートを変更

--- src/model/auth.go
+++ src/model/auth.go
@@ -4,5 +4,5 @@ package model
 type AuthPostRequest struct {
 	ServiceType string `json:"service_type" validate:"required"`
 	ServiceId   string `json:"service_id" validate:"required"`
-	Email       string `json:"email"`
+	Email       string `json:"email,omitempty"`
 }
